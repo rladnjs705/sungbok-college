3fa540bbd280c14f4b21c48bf4f1bace5ccc9290
jaewon705 <rladnjs705@naver.com>
2023-03-16 11:00:44 +0900

    1.disqus테스트

--- src/routes/(app)/articles/[id]/+page.svelte
+++ src/routes/(app)/articles/[id]/+page.svelte
@@ -4,14 +4,12 @@
     const id = $page.params.id;
 
 </script>
-<div id="disqus_thread"></div>
+<!-- <div id="disqus_thread"></div> -->
 <Disqus
   shortname="sungbok"
   identifier={id}
   title="새벽이슬"
   url="https://port-0-sungbok-college-1jx7m2gldi7x7be.gksl2.cloudtype.app"
+  lang="ko"
 />
-<svelte:head>
-  <script src="https://sungbok.disqus.com/embed.js" async defer></script>
-</svelte:head>
 {id}
