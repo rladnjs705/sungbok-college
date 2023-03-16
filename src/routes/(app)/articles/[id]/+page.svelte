1d8b961431940063014c8be8d5b5a5f5d3e2eb4f
jaewon705 <rladnjs705@naver.com>
2023-03-16 11:51:26 +0900

    1.disqus 제거

--- src/routes/(app)/articles/[id]/+page.svelte
+++ src/routes/(app)/articles/[id]/+page.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
     import { page } from "$app/stores";
-    import Disqus from 'svelte-disqus';
     const id = $page.params.id;
 
 </script>
-<!-- <div id="disqus_thread"></div> -->
-<Disqus
-  shortname="sungbok"
-  identifier={id}
-  title="새벽이슬"
-  url="https://port-0-sungbok-college-1jx7m2gldi7x7be.gksl2.cloudtype.app"
-  language="ko"
-/>
 {id}
