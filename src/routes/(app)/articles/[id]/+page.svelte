a37c76517ec79ece08321ee27b2d01d743c5527d
jaewon705 <rladnjs705@naver.com>
2023-03-16 10:55:20 +0900

    1.disqus다른방식으로해보기

--- src/routes/(app)/articles/[id]/+page.svelte
+++ src/routes/(app)/articles/[id]/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
     import { page } from "$app/stores";
-    //import Disqus from 'svelte-disqus';
+    import Disqus from 'svelte-disqus';
     const id = $page.params.id;
 
 </script>
 <div id="disqus_thread"></div>
-<!-- <Disqus
+<Disqus
   shortname="sungbok"
   identifier={id}
   title="새벽이슬"
   url="https://port-0-sungbok-college-1jx7m2gldi7x7be.gksl2.cloudtype.app"
-/> -->
-<!-- <svelte:head>
+/>
+<svelte:head>
   <script src="https://sungbok.disqus.com/embed.js" async defer></script>
-</svelte:head> -->
+</svelte:head>
 {id}
