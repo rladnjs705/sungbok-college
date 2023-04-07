a30f7da5c11ce502f605dc9c6dc5c65a3977a4aa
jaewon705 <rladnjs705@naver.com>
2023-04-07 14:05:45 +0900

    1.강의 콘텐츠 ui 수정

--- src/lib/components/board/LectureDetailList.svelte
+++ src/lib/components/board/LectureDetailList.svelte
@@ -6,6 +6,7 @@
     import lectureSvg from "$lib/images/work_from_home-remove.png";
     import patternSvg from "$lib/images/pattern.svg";
     import profileSvg from "$lib/images/profile.png";
+    import noImage from "$lib/images/noImage.jpg";
     import { LECTURE } from '$lib/utils/constans';
     
     export let list:any;
@@ -55,15 +56,6 @@
         if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T'
     }
 
-    //xss 역치환
-    function convertHtml(str:string){
-        str = str.replace(/&lt;/g,"<");
-        str = str.replace(/&gt;/g,">");
-        str = str.replace(/&quot;/g,"\"");
-        str = str.replace(/&#39;/g,"\'");
-        return str;
-    }
-
     $:
     {
         if($boardDetailList){
@@ -74,13 +66,9 @@
         }
     }
 
-    const onCategorySelected = (categoryEng:string) =>{
-        $pageNumber = 1;
-        itemCategorySelected.selectCategory(categoryEng);
-        boardDetailList.getBoardDetailList(boardType,categoryEng);
-        goto(`/${boardType}/${categoryEng}`)
+    const errorImage = (e:any) => {
+        e.target.src = noImage;
     }
-    
 </script>
 
 <div class="w-full mt-5">
@@ -108,23 +96,93 @@
         </div>
     </a>
 
-        {#if categories}
-            <Category boardType={boardType} categories={categories} />
+    {#if categories}
+        <Category boardType={boardType} categories={categories} />
+    {/if}
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+        {#if paginatedItems.length > 0}
+            {#each paginatedItems.reverse() as lecture}
+                <div class="w-full space-y-[9px]">
+                    <a
+                        href="/articles/{lecture.id}"
+                        class="line-clamp-1 w-full break-all rounded-md bg-gray-50 hover:cursor-pointer"
+                        ><img
+                            class="h-40 w-full rounded-md border border-gray-300 pt-4 pb-4"
+                            src={lecture.thumbnailPath}
+                            alt="thumbnail"
+                            on:error|once={errorImage}
+                        /></a
+                    >
+                    <div class="flex justify-between text-xs font-normal">
+                        <div class="flex items-center space-x-1">
+                            <!-- <a class="line-clamp-1 break-all text-gray-700 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-200"
+                                href="/users/{lecture.writer.userId}">{lecture.writer.nickName}</a> -->
+                            <div class="line-clamp-1 break-all text-gray-700 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-200">
+                                {lecture.teacher}
+                            </div>
+                            <span class="h-0.5 w-0.5 rounded-full bg-gray-700 dark:bg-gray-300" />
+                            <span class="line-clamp-1 break-all text-gray-700 dark:text-gray-300">
+                                {displayedAt(lecture.createDate)}
+                            </span>
+                        </div>
+                        <div class="inline-flex items-center space-x-0.5 text-xs sm:text-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-5 w-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            </svg>
+                            <span class="font-medium">{formatLargeNumber(lecture.viewCount)}</span>
+                            <span class="sr-only">views</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-5 w-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                            </svg>
+                            <span class="font-medium">{formatLargeNumber(lecture.commentCount)}</span>
+                            <span class="sr-only">comments</span>
+                        </div>
+                    </div>
+                    <a
+                        class="block truncate font-semibold text-gray-900 hover:text-blue-500 dark:text-gray-100 dark:hover:text-blue-200"
+                        href="/articles/{lecture.id}"
+                        >{lecture.title}</a
+                    >
+                </div>
+            {/each}
+            {:else}
+            <div class="text-center">게시물이 없습니다.</div>
         {/if}
-    <div>
+    </div>
+    {#if $isDark}
+        <DarkPaginationNav
+            totalItems="{response.totalElements}"
+            pageSize="{pageSize}"
+            currentPage="{currentPage}"
+            limit="{1}"
+            showStepOptions="{true}"
+            on:setPage="{(e) => currentPage = e.detail.page}"
+        />
+        {:else}
+        <LightPaginationNav
+            totalItems="{response.totalElements}"
+            pageSize="{pageSize}"
+            currentPage="{currentPage}"
+            limit="{1}"
+            showStepOptions="{true}"
+            on:setPage="{(e) => currentPage = e.detail.page}"
+        />
+    {/if}
+    <!-- <div>
         <ul class="divide-y divide-gray-500/30 dark:divide-gray-500/70">
             {#if paginatedItems}
             {#each paginatedItems as board}
             <li class="py-4 last:pb-0">
                 <div class="mb-2 flex">
                     <div class="flex flex-1 items-center space-x-1">
-                        <!-- <a href="/users/138400"> -->
+                        <a href="/users/138400">
                             <img
                                 class="h-5 w-5 rounded-full"
                                 src={profileSvg}
                                 alt="프로필 사진"
                             />
-                        <!-- </a> -->
+                        </a>
                         <div
                             class="inline pl-1 text-xs font-medium text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-200 sm:text-sm"
                             >{board.teacher}</div
@@ -182,13 +240,13 @@
                             <span class="font-medium">{formatLargeNumber(board.commentCount)}</span>
                             <span class="sr-only">comments</span>
                         </div>
-                        <!-- <div class="inline-flex items-center -space-x-0.5 text-xs sm:text-sm">
+                        <div class="inline-flex items-center -space-x-0.5 text-xs sm:text-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-5 w-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"></path>
                             </svg>
                             <span class="font-medium">{formatLargeNumber(board.likeCount)}</span>
                             <span class="sr-only">likes</span>
-                        </div> -->
+                        </div>
                     </div>
                 </div>
             </li>
@@ -219,5 +277,5 @@
             {/if}
             
         </ul>
-    </div>
+    </div> -->
 </div>
--- src/lib/components/board/LectureList.svelte
+++ src/lib/components/board/LectureList.svelte
@@ -36,6 +36,14 @@
         return yyyy + "-" + MM + "-" + dd + " " + HH + ":" + mm + ":" + ss;
     };
 
+    const formatLargeNumber = (n:number) => {
+        if (n < 1e3) return n
+        if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K'
+        if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'M'
+        if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'B'
+        if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T'
+    }
+
     const errorImage = (e:any) => {
         e.target.src = noImage;
     }
@@ -91,9 +99,19 @@
                                 {displayedAt(lecture.createDate)}
                             </span>
                         </div>
-                        <!-- <div class="line-clamp-1 break-all text-gray-700 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-200">
-                            {lecture.teacher}
-                        </div> -->
+                        <div class="inline-flex items-center space-x-0.5 text-xs sm:text-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-5 w-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            </svg>
+                            <span class="font-medium">{formatLargeNumber(lecture.viewCount)}</span>
+                            <span class="sr-only">views</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-5 w-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                            </svg>
+                            <span class="font-medium">{formatLargeNumber(lecture.commentCount)}</span>
+                            <span class="sr-only">comments</span>
+                        </div>
                     </div>
                     <a
                         class="block truncate font-semibold text-gray-900 hover:text-blue-500 dark:text-gray-100 dark:hover:text-blue-200"
